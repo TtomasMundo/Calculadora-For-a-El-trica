76ba7c0014a3d31c158d639ee555a27f48f320c6
TtomasMundo <165791055+TtomasMundo@users.noreply.github.com>
2024-10-08 08:31:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,9 +5,23 @@ function calcular(){
     var d = parseFloat(document.getElementById('d').value);
 
     var Força = (K * Q * q) / (d*d);
+   
 
     document.getElementById('Força').innerText = Força;
 }
+
+function calcular2(){
+    var q2 = parseFloat(document.getElementById('q2').value);
+    var v = parseFloat(document.getElementById('v').value);
+    var B = parseFloat(document.getElementById('B').value);
+    var sen = parseFloat(document.getElementById('sen').value);
+
+    var ForçaM = (q2 * v * B  * sen);
+   
+
+    document.getElementById('ForçaM').innerText = ForçaM;
+
+    }
 
 
 
